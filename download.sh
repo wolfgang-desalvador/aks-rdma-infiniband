9892f08dfb8305d624c4b4c58d80bfc575438a97
Wolfgang De Salvador <wolfgang.desalvador@microsoft.com>
2023-04-12 13:58:39 +0200

    Fixx double dash

--- download.sh
+++ download.sh
@@ -2,4 +2,4 @@
 
 apt update && apt install -y curl
 
-curl -L https://content.mellanox.com/ofed/MLNX_OFED-<mellanox_version>/MLNX_OFED_LINUX--<mellanox_version>-ubuntu<ubuntu_version>-x86_64.iso -o MLNX_OFED_LINUX--<mellanox_version>-ubuntu<ubuntu_version>-x86_64.iso
+curl -L https://content.mellanox.com/ofed/MLNX_OFED-<mellanox_version>/MLNX_OFED_LINUX-<mellanox_version>-ubuntu<ubuntu_version>-x86_64.iso -o MLNX_OFED_LINUX-<mellanox_version>-ubuntu<ubuntu_version>-x86_64.iso
